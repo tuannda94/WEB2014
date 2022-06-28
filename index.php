c7c26d7e0f46af264644d206707190f0563a91ff
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-06-28 11:17:51 +0700

    Buoi 1 Su dung PHP + HTML

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Vùng nằm ngoài phạm vi PHP sẽ là HTML thuần -->
+Vùng chứa code PHP
+
+<?php
+    // Vùng chứa code PHP
+    // echo trong PHP dùng để hiển thị nội dung lên trình duyệt
+    echo 'Vùng chứa code PHP';
+
+    // Khai báo biến trong PHP
+    $a = 123; // Khai báo biến a và gán giá trị bằng số 123
+    $b = 'abc'; // Khai báo biến b và gán giá trị bằng chuỗi 'abc'
+    $c = null;
+    $d = true;
+    // $e = undefined; // Khi kiểu dữ liệu là undefined thì sẽ chưa sử dụng được
+    echo $a, $b, $c, $d;
+?>
