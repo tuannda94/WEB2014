59543c464eb17bdbaa5e80867a37bec868272a9d
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-11-14 12:47:25 +0700

    Luyen tap PHP - HTML

---
+++ php-html-2.php
@@ -0,0 +1,50 @@
+<!-- Tạo giao diện hiển thị danh sách sản phẩm kết hợp PHP -->
+<?php
+$danh_muc_1 = 'Điện thoại';
+$ds_dien_thoai = [
+    [
+        'id' => 1,
+        'name' => 'IPHONE 12',
+        'price' => 13000000,
+    ],
+    [
+        'id' => 1,
+        'name' => 'IPHONE 12',
+        'price' => 13000000,
+    ],
+    [
+        'id' => 2,
+        'name' => 'IPHONE 13',
+        'price' => 13000000,
+    ],
+    [
+        'id' => 3,
+        'name' => 'IPHONE 14',
+        'price' => 13000000,
+    ],
+    [
+        'id' => 4,
+        'name' => 'IPHONE 15',
+        'price' => 13000000,
+    ],
+];
+
+$danh_muc_2 = 'Máy tính';
+$ds_may_tinh = [
+    [
+        'id' => 1,
+        'name' => 'DELL XPS',
+        'brand' => 'DELL'
+    ],
+    [
+        'id' => 2,
+        'name' => 'ACER',
+        'brand' => 'ACER'
+    ],
+    [
+        'id' => 3,
+        'name' => 'RAZER',
+        'brand' => 'RAZER'
+    ],
+];
+?>
